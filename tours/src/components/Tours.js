4bd64e3d7b32ef37d9e8d07841903bdef0170d3e
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-01 18:22:58 +1000

    upload build

--- tours/src/components/Tours.js
+++ tours/src/components/Tours.js
@@ -6,7 +6,7 @@ const Tours = ({tours, removedList}) => {
 <h1 className="title"> Kripu's Tours Gallery</h1>
 
 <div className="bar"></div>
-<h2 className="tours-count"> {tours.length>0 ? <h2> There are {tours.length} tour option avaliable</h2> : <h2> Please visit back later there are no tour option available</h2>}</h2>
+<h2 className="tours-count"> {tours.length>0 ? <h2> There are {tours.length} tour options avaliable</h2> : <h2> Please visit back later there are no tour option available</h2>}</h2>
 
 {
     tours.map((value)=>{
