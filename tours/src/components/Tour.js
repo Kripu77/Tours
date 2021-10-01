4dcf0ada4cd2cc0ca37d69148e0bdf71bb44b6c9
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-01 20:24:23 +1000

    final updates

--- tours/src/components/Tour.js
+++ tours/src/components/Tour.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Tour = ({id, image, info, name, price, removedTour}) => {
-
+const[readMore, setReadMore] = useState(false);
     return <article><div className="main-card">
         <img src={image} alt={name}/>
     
@@ -9,7 +9,11 @@ const Tour = ({id, image, info, name, price, removedTour}) => {
                <h1> {name}</h1> 
                <h1 className="price">${price}</h1>
         </div>
-        <p>{info}</p>
+        {/* the info.substring method shows only 200 texts */}
+      <p>
+      {readMore? info : `${info.substring(0,220)}.......`}
+      <button className="toggle-btn" onClick={()=>setReadMore(!readMore)}> {readMore?` Show Less` : `Read More`}</button>
+      </p>
          <button className="delete-btn" onClick={()=>removedTour(id)}> Not Intrested</button>
         </div>
        
